a7cdebf2a074a41bd855c0f14489c1f66d030d97
MinSang22Kim <powerminsang9901@gmail.com>
2024-04-10 22:49:02 +0900

    MVC 패턴 - 적용 (회원 조회)

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -14,10 +14,12 @@ import java.util.List;
 
 @WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
 public class MvcMemberListServlet extends HttpServlet {
+
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         List<Member> members = memberRepository.findAll();
         request.setAttribute("members",members);
 
